c9879919eba772b03619c31cf475de5285960236
Leon <leon.kushnir15@gmail.com>
2024-12-16 20:35:19 +0200

    change pillow version

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
